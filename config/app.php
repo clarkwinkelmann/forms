e8631b47c4c7b10ce06b30e1a13b6c1a65c2ba99
Clark Winkelmann <clark.winkelmann@gmail.com>
2016-03-03 15:30:20 +0100

    Added TrustedProxy package All my installs need it so I think it's better to have it by default

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
+
     ],
 
     /*
