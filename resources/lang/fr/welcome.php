365dad1e2a141bb47ebe9120bd1ec48ee91f6894
Clark Winkelmann <clark.winkelmann@gmail.com>
2016-11-29 18:41:06 +0100

    Fix typo

--- resources/lang/fr/welcome.php
+++ resources/lang/fr/welcome.php
@@ -9,7 +9,7 @@
 return [
 
 	'title' => 'Bienvenue étranger !',
-	'body'  => 'Vous être arrivé sur une application qui récolte des réponses à des formulaires sur internet' . "\n" .
+	'body'  => 'Vous êtes arrivé sur une application qui récolte des réponses à des formulaires sur internet' . "\n" .
 			'Je crais que vous ne puissiez faire grand chose ici =(',
 
 	'find_code' => 'Cette application est open source. Vous trouverez le code source sur :link',
